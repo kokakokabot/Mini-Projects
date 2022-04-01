8cbf7a9261d78700b48c1db491744c0ec5c17752
kokakokabot <54101416+kokakokabot@users.noreply.github.com>
2022-04-01 13:37:54 +0200

    Football Game

--- Football Game.py
+++ Football Game.py
@@ -1,1 +1,40 @@
+import os, time, random
 
+class Team:
+    def __init__(self,name):
+        self.name = name 
+
+class Score:
+    home = 0
+    away = 0
+
+class Match:
+    def __init__(self, t1, t2):
+        self.home = t1
+        self.away = t2
+        self.minute = 0
+        self.score = Score()
+    def getGoals(self):
+        return random.randint(1,100) == 5, random.randint(1,100) == 5
+    def start(self):
+        self.minute = 0
+        while True:
+            os.system("cls")
+            print(f"{self.home.name} vs {self.away.name}")
+            g1, g2 = self.getGoals()
+            self.score.home += 1 if g1 else 0
+            self.score.away += 1 if g2 else 0
+            self.print_score()
+            self.minute += 1
+            time.sleep(0.2)
+            if self.minute > 90:
+                print("Match Ended")
+                exit()
+    def print_score(self):
+        print(f"{self.score.home} - {self.score.away} ({self.minute} minute)")
+
+t1 = Team("Zeljeznicar")
+t2 = Team("Sarajevo")
+
+match = Match(t1,t2)
+match.start()
